d67e8a6286d4700541f8aba8d26f5094d1914537
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-09-30 08:53:22 +1000

    2.4 Push

--- src/first.rs
+++ src/first.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(clippy::new_without_default)]
 
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -18,5 +20,29 @@ struct Node {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    // Not part of the 2.4 Push chapter but I initially interpretted "push" as an
+    // append operation and tried to implement it without looking at the book's code.
+    pub fn append(&mut self, elem: i32) {
+        let mut cur_link = &mut self.head;
+        while let Link::More(cur_node) = cur_link {
+            cur_link = &mut cur_node.next;
+        }
+
+        let new_node = Box::new(Node {
+            elem,
+            next: Link::Empty,
+        });
+        *cur_link = Link::More(new_node);
     }
 }

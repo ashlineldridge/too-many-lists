7a48b2ab01db263494e880cccf4cd8c8360d882c
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2021-10-01 07:08:25 +1000

    2.7 Drop

--- src/first.rs
+++ src/first.rs
@@ -57,6 +57,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    // For explanation see https://rust-unofficial.github.io/too-many-lists/first-drop.html.
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
